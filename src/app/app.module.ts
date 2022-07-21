2744241328f2df4fc113da647a62ba7f914a833e
anilakodukula <anilakodukula@gmail.com>
2022-07-20 21:57:09 -0400

    getting started with angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({ // directives use modules for organization
   declarations: [//declares the app component so that angular can locate its selector
-    AppComponent // properties are arrays
+    AppComponent, // properties are arrays
+    ProductListComponent
   ],
   imports: [
     BrowserModule //external modules that registers important application service providers such as error handling.
